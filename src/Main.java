ed0daed498de8664e2b4d77739a594ba25fe0fcc
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-19 14:07:30 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello rld!");
+        task1();
+
     }
+
+    public static void hightYear(int year) {
+        int multipleOfFour = year % 4;
+        int multipleOfOnehundred = year % 100;
+        int multipleOfFourhundred = year % 400;
+        if (multipleOfOnehundred != 0 && multipleOfFour == 0 || multipleOfFourhundred == 0) {
+            System.out.println(year + " год - високосный");
+        } else {
+            System.out.println(year + " год - не високосный");
+        }
+    }
+
+    public static void task1() {
+        System.out.println("Введите год");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        hightYear(year);
+    }
+
 }
